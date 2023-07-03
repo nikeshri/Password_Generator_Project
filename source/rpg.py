acfaa968fca0396dea441bcedbaa4f8d73a9cb7f
unknown <mishranikesh317@gmail.com>
2023-07-03 17:55:45 +0545

    first commit

---
+++ source/rpg.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import string
+import random
+
+def random_password_generator():
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    size = 8
+    return ''.join(random.choice(chars) for x in range(size, 20))
+
+def random_password_generator_ico():
+	random_password_generator_ico = """
+	#############################################################
+	# PYTHON - Random Password Generetor (RPG) - Nikesh Mishra #
+	#############################################################
+	#                         CONTACT                           #
+	#############################################################
+	#               DEVELOPER : Nikesh mishra                #
+	#          Mail Address : mishranikesh317@gmail.com         #
+	#     #
+	#                           #
+	#############################################################
+	"""
+	print(random_password_generator_ico)
